75516bda4c35e416d464005f50c0e35f585389be
Luis <reescolando@gmail.com>
2018-10-09 23:59:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Compilar main.cpp com:
+
+g++ main.cpp -L /usr/local/lib/ `pkg-config --libs opencv` -lrealsense -g -Wall -Wextra
---
+++ main.cpp
@@ -0,0 +1,153 @@
+#include <thread>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <librealsense/rs.hpp>
+
+uint16_t* filterData(uint16_t* data, int width, int height, uint16_t max, uint16_t min) {
+  static uint16_t filtered[9999999];
+  int i;
+  // uint16_t max, min;
+  // max & 0000000000000000;
+  // min | 1111111111111111;
+  for (i=0;i<(width*height);i++) {
+    if (data[i] >= max) {
+      filtered[i] = 0;
+    } else if (data[i] <= min) {
+      filtered[i] = 0;
+    } else {
+      filtered[i] = 65535;
+    }
+  }
+
+  return filtered;
+
+}
+
+double getAxis(cv::Mat data, int filter) {
+  int i, j;
+  int left = 0;
+  int right = 0;
+  int nLeft = 0;
+  int nRight = 0;
+  float meiota = data.cols/2;
+  // uint16_t max, min;
+  // max & 0000000000000000;
+  // min | 1111111111111111;
+
+  for(i = 0; i < (data.rows - filter); i++) {
+    // uchar* Mi = data.ptr(i);
+    if (i > filter) {
+      for(j = 0; j < (data.cols - filter); j++) {
+        // std::cout << Mi[j] << std::endl;
+        if (j > filter) {
+          if (data.at<int>(i,j) != 0) {
+            if (j <= meiota) { // está na esquerda
+              nLeft = nLeft + 1;
+              left = left + i;
+            } else { // está na direita
+              nRight = nRight + 1;;
+              right = right + i;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  // for (i=0;i<(width*height);i++) {
+  //   if (data[i] > 0) {
+  //     if ((i%width < meiota)) { // está na esquerda
+  //       nLeft ++;
+  //       left += i/width;
+  //     } else { // está na direita
+  //       nRight ++;
+  //       right += i/width;
+  //     }
+  //   }
+  // }
+
+  double mLeft = left/(nLeft+1);
+  double mRight = right/(nRight+1);
+
+  // std::cout << "Left " << " " << left << " " << nLeft << " " << mLeft << std::endl ;
+  // std::cout << "Right " << " " << right << " " << nRight << " " << mRight << std::endl ;
+
+  return (double)(mLeft - mRight);
+}
+
+int main(int argc, char** argv)
+{
+    rs::context ctx;
+    if(ctx.get_device_count() == 0) return EXIT_FAILURE;
+    rs::device * dev = ctx.get_device(0);
+
+    const uint16_t meter = static_cast<uint16_t>(1.0f/dev->get_depth_scale());
+
+    std::cout << meter << std::endl;
+
+    dev->enable_stream(rs::stream::depth, 0, 0, rs::format::z16, 60);
+    // rs::device is NOT designed to be thread safe, so you should not currently make calls
+    // against it from your callback thread. If you need access to information like 
+    // intrinsics/extrinsics, formats, etc., capture them ahead of time
+    rs::intrinsics depth_intrin, color_intrin;
+    rs::format depth_format, color_format;
+    depth_intrin = dev->get_stream_intrinsics(rs::stream::depth);
+    depth_format = dev->get_stream_format(rs::stream::depth);
+
+    // Set callbacks prior to calling start()
+    cv::Mat frameGray;
+    cv::Mat frameGrayNrm;
+    auto depth_callback = [depth_intrin, depth_format, &frameGray, &frameGrayNrm](rs::frame f)
+    {
+      cv::Mat frame(cv::Size(depth_intrin.width,depth_intrin.height), CV_16UC1, filterData((uint16_t*)f.get_data(),depth_intrin.width,depth_intrin.height,750,500));
+      frame.convertTo(frameGray, CV_8U, 1. / 64);
+      int i;
+      for (i=0;i<(depth_intrin.width*depth_intrin.height);i++) {
+
+      }
+      // cv::normalize(frameGray, frameGrayNrm, 0, 256, cv::NORM_MINMAX);
+      // const int channelMap[] = { 0,0, 0,1, 0,2 };
+      // cv::mixChannels(&frame, 1, &frameColor, 1, channelMap, 3);
+
+      // int count = 0;
+      // cv::MatConstIterator_<int> it = frame.begin<int>(), it_end = frame.end<int>();
+      // for(; it != it_end; ++it) {
+      //   std::cout << *it << std::endl;
+      // }
+
+    };
+    dev->set_frame_callback(rs::stream::depth, depth_callback);
+
+    // Between start() and stop(), you will receive calls to the specified callbacks from background threads
+    dev->start();
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    int frames = 0;
+    int axisHistory[5] = {0,0,0,0,0};
+
+    while (1) {
+      cv::Mat rgb;
+      // cv::applyColorMap(frameGray, rgb, cv::COLORMAP_COOL);
+      cv::morphologyEx(frameGray, rgb, cv::MORPH_OPEN, cv::Mat());
+      cv::morphologyEx(rgb, rgb, cv::MORPH_CLOSE, cv::Mat());
+      // cv::morphologyEx(rgb, rgb, cv::MORPH_GRADIENT, cv::Mat());
+      cv::imshow("this is you, smile! :)", rgb);
+
+      if (frames == 6) {
+        int i, soma = 0;
+        double value;
+        for (i=0;i<=frames;i++){
+          soma += axisHistory[i];
+        }
+        value = soma/frames;
+        frames = 0;
+        std::cout << value << std::endl ;
+        
+      } else {
+        axisHistory[frames++] = getAxis(rgb, 30);
+      }
+      cv::waitKey(1);
+    }
+    dev->stop();
+    return 0;
+}
